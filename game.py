1a093c59718098adee58083a9fd0d5df0c095746
kornel43 <kornel43@gmail.com>
2019-01-26 17:19:33 +0100

    added link to sprites

--- game.py
+++ game.py
@@ -4,68 +4,41 @@ Module provides game itself
 You can download needed sprites from here: https://www.mediafire.com/file/cb365fxtu8ivt2u/sprites.7z/file
 """
 import math
-import numpy
 import os
-
 import random as r
 
+import numpy
 import pygame
 from pygame.math import Vector2
 
 from rocket import load_sprites
+from text_styler import create_text
 
 x = 500
 y = 40
-black = (255, 255, 255)
+
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x, y)
-
-
-def make_font(fonts, size):
-    available = pygame.font.get_fonts()
-    choices = map(lambda font: font.lower().replace(' ', ''), fonts)
-    for choice in choices:
-        if choice in available:
-            return pygame.font.SysFont(choice, size)
-    return pygame.font.Font(None, size)
-
-
-_cached_fonts = {}
-
-
-def get_font(font_preferences, size):
-    global _cached_fonts
-    key = str(font_preferences) + '|' + str(size)
-    font = _cached_fonts.get(key, None)
-    if font is None:
-        font = make_font(font_preferences, size)
-        _cached_fonts[key] = font
-    return font
-
-
-_cached_text = {}
-
-
-def create_text(text, fonts, size, color):
-    global _cached_text
-    key = '|'.join(map(str, (fonts, size, color, text)))
-    image = _cached_text.get(key, None)
-    if image is None:
-        font = get_font(fonts, size)
-        image = font.render(text, True, color)
-        _cached_text[key] = image
-    return image
-
-
-font_preferences = ["Papyrus", "Comic Sans MS"]
+colors = {
+    'red': (255, 0, 0),
+    'black': (0, 0, 0),
+    'white': (255, 255, 255),
+    'green': (0, 255, 0),
+    'blue': (0, 0, 255),
+    'pink': (255, 51, 153),
+    'yellow': (255, 255, 0),
+    'brown': (128, 0, 0),
+    'purple': (153, 0, 255),
+}
 
 
 class Game:
     def __init__(self, size, rocket, sprites):
-        self.chooses = [0, 0, 0] # wybory w opcjach, ale uwaga, to lista list
+        self.chooses = [0, 0, 0]  # wybory w opcjach, ale uwaga, to lista list
         self.sprites = sprites
         self.sprites_len = len(sprites[0])
         self.image = sprites[0][self.sprites_len // 2]
         self.max_x, self.max_y = size
+        self.font = ["Comic Sans MS"]
         self.gravity = 10
         self.air_res = 0.8
         self.pos = None
@@ -79,6 +52,7 @@ class Game:
         self.tick_time = 60
         self.pause = False
         self.is_force = False
+        self.background_color = colors['black']
 
     def init_pos(self):
         self.pos = Vector2(self.max_x / 2, self.max_y / 4)
@@ -116,14 +90,14 @@ class Game:
                     self.done = True
                     not_chosen = False
 
-            screen.fill(black)
+            screen.fill(self.background_color)
             for i, text in enumerate(texts):
                 if i == option:
                     color = (0, 155, 0)
                 else:
                     color = (0, 0, 255)
                 font_size = 60
-                new_text = create_text(text, font_preferences, font_size, color)
+                new_text = create_text(text, self.font, font_size, color)
                 x_pos = (self.max_x - new_text.get_width()) // 2
                 y_pos = (1 + i) * (self.max_y - new_text.get_height()) // (2 + text_len) + font_size
                 screen.blit(new_text, (x_pos, y_pos))
@@ -142,21 +116,21 @@ class Game:
                     if event.key in (pygame.K_SPACE, pygame.K_RETURN, pygame.K_ESCAPE):
                         is_about = False
 
-            screen.fill(black)
-            colors = [(0, 0, 255), (255, 0, 0), (0, 255, 0), (0, 125, 125), (125, 125, 0)]
-            for i, text in enumerate(texts):
+            screen.fill(self.background_color)
+            about_colors = [color for color in colors.values() if color != self.background_color]
+            for i, text in enumerate(zip(texts, about_colors)):
                 font_size = 60
-                new_text = create_text(text, font_preferences, font_size, colors[i])
+                new_text = create_text(text[0], self.font, font_size, text[1])
                 x_pos = (self.max_x - new_text.get_width()) // 2
                 y_pos = (1 + i) * (self.max_y - new_text.get_height()) // (2 + text_len) + font_size
                 screen.blit(new_text, (x_pos, y_pos))
             pygame.display.flip()
 
-    def option_menu(self,screen):
+    def option_menu(self, screen):
         not_chosen = True
         option_meteors = ["Meteors: easy", "Meteors: medium", "Meteors: hard", "Meteors: impossible"]
-        options1 = ['raz','dwa','tzy']
-        options2 = ['Kornel','gra','w','pedalskie','gry']
+        options1 = ['raz', 'dwa', 'tzy']
+        options2 = ['Kornel', 'gra', 'w', 'pedalskie', 'gry']
         options = [option_meteors, options1, options2]
         self.chooses_len = []
         for option in options:
@@ -174,7 +148,7 @@ class Game:
                     elif event.key == pygame.K_LEFT:
                         self.chooses[option] = (self.chooses[option] - 1) % self.chooses_len[option]
                     elif event.key == pygame.K_RIGHT:
-                       self. chooses[option] = (self.chooses[option] + 1) % self.chooses_len[option]
+                        self.chooses[option] = (self.chooses[option] + 1) % self.chooses_len[option]
                     elif event.key in (pygame.K_SPACE, pygame.K_RETURN):
                         if option == 0:
                             self.play_game(screen)
@@ -192,46 +166,46 @@ class Game:
                     self.done = True
                     not_chosen = False
 
-            screen.fill(black)
-            color = (0, 0, 255)#, (255, 0, 0), (0, 255, 0), (0, 125, 125), (125, 125, 0)]
+            screen.fill(self.background_color)
+            color = (0, 0, 255)  # , (255, 0, 0), (0, 255, 0), (0, 125, 125), (125, 125, 0)]
             for i, opt in enumerate(options):
                 color = (0, 0, 255)
                 font_size = 60
                 what_to_write = opt[self.chooses[i]]
                 if i == option:
-                    #what_to_write = "-> " + what_to_write
-                    color = (0,255,0)
-                new_text = create_text(what_to_write, font_preferences, font_size, color)
+                    # what_to_write = "-> " + what_to_write
+                    color = (0, 255, 0)
+                new_text = create_text(what_to_write, self.font, font_size, color)
                 x_pos = (self.max_x - new_text.get_width()) // 2
                 y_pos = (1 + i) * (self.max_y - new_text.get_height()) // (2 + text_len) + font_size
                 screen.blit(new_text, (x_pos, y_pos))
             pygame.display.flip()
 
     def show_speed(self, screen):
-        speed_x_val = abs(round(self.acc.x  / (2 / 173)))
-        speed_y_val = abs(round(self.acc.y  / (2 / 173)))
-        speed_x = create_text('Horizontal:  {} km/h'.format(speed_x_val), font_preferences, 20, (255, 0, 0))
-        speed_y = create_text('Vertical:  {} km/h'.format(speed_y_val), font_preferences, 20, (255, 0, 0))
+        speed_x_val = abs(round(self.acc.x / (2 / 173)))
+        speed_y_val = abs(round(self.acc.y / (2 / 173)))
+        speed_x = create_text('Horizontal:  {} km/h'.format(speed_x_val), self.font, 20, (255, 0, 0))
+        speed_y = create_text('Vertical:  {} km/h'.format(speed_y_val), self.font, 20, (255, 0, 0))
         screen.blit(speed_x, (10, 30))
         screen.blit(speed_y, (10, 10))
-        
+
     def generate_surface(self, n, surface_length, rng):
         height = 1000
         width = 1400
         surf = []
-        landing_site = [numpy.random.randint(surface_length,n-surface_length), height - numpy.random.randint(0, rng)]
-        self.landing_site = [(landing_site[0]-surface_length)*width//n, (landing_site[0]+surface_length)*width//n]
+        landing_site = [numpy.random.randint(surface_length, n - surface_length), height - numpy.random.randint(0, rng)]
+        self.landing_site = [(landing_site[0] - surface_length) * width // n,
+                             (landing_site[0] + surface_length) * width // n]
         for i in range(n):
-            if abs(i-landing_site[0])<=surface_length:
-                surf.append([i*width//n, landing_site[1]])
+            if abs(i - landing_site[0]) <= surface_length:
+                surf.append([i * width // n, landing_site[1]])
             else:
-                surf.append((i*width//n, height - numpy.random.randint(0, rng)))
-        surf.append((width-1,height))
+                surf.append((i * width // n, height - numpy.random.randint(0, rng)))
+        surf.append((width - 1, height))
         self.surface = [0] * width
-        print(self.landing_site)
         for i in range(n):
-            for j in range(width//n):
-                self.surface[i*width//n+j] = (surf[i][1] * (width//n-j) + surf[i+1][1] * j)//(width//n)
+            for j in range(width // n):
+                self.surface[i * width // n + j] = (surf[i][1] * (width // n - j) + surf[i + 1][1] * j) // (width // n)
         return surf
 
     def run(self):
@@ -247,8 +221,8 @@ class Game:
         self.init_pos()
         self.reset_acc()
         self.reset_meteors()
-        planet_surface = self.generate_surface(100,5,500)
-        #pygame.mixer.music.play()
+        planet_surface = self.generate_surface(100, 5, 500)
+        # pygame.mixer.music.play()
         while not self.done:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -260,7 +234,7 @@ class Game:
             self.add_gravity()
             self.add_air_resistance()
             self.change_rocket_position()
-            screen.fill(black)
+            screen.fill(self.background_color)
             pygame.draw.lines(screen, 0, 0, planet_surface, 10)
             self.draw_meteors(screen)
             self.draw_rocket(screen)
@@ -286,19 +260,18 @@ class Game:
         for i, [x, y, x_acc, y_acc] in enumerate(self.list_of_meteors):
             self.list_of_meteors[i][0] += x_acc
             self.list_of_meteors[i][1] += y_acc
-            #print(x,' ',y)
             self.meteor = pygame.Rect(x, y, self.image.get_width(), self.image.get_height())
-            pygame.draw.rect(screen, black, self.meteor)
+            pygame.draw.rect(screen, self.background_color, self.meteor)
             screen.blit(self.image, self.meteor)
 
     def add_meteor(self):
         mid_or_feed = r.random() < 0.8
-        if(mid_or_feed):
+        if (mid_or_feed):
             x = 0
             y = r.random() * self.max_y
         else:
             x = r.random() * self.max_x / 3  # na wysokosci 1/3
-            y = self.max_y * ( r.random() < 1/2) # z lewej albo z prawej
+            y = self.max_y * (r.random() < 1 / 2)  # z lewej albo z prawej
         x_acc = r.random() * 2
         y_acc = r.random() * 2
         meteor = [x, y, x_acc, y_acc]
@@ -310,14 +283,15 @@ class Game:
         if not 0 < self.rocket.y < self.max_y - self.rocket.height:
             self.not_lost_game = True
         if not self.rocket.y <= self.surface[self.rocket.x] - self.rocket.height:
-            if abs(self.acc.x)<2 and abs(self.acc.y)<5 and self.landing_site[0] < self.rocket.x < self.landing_site[1]:
+            if abs(self.acc.x) < 2 and abs(self.acc.y) < 5 and self.landing_site[0] < self.rocket.x < self.landing_site[
+                1]:
                 self.won_game = True
             else:
                 self.not_lost_game = True
         for i in self.list_of_meteors:
             for j in self.hitbox:
                 radius = 70
-                if ((i[0]+radius) - j[0]) ** 2 + ((i[1]+radius) - j[1]) **2 <= radius ** 2:
+                if ((i[0] + radius) - j[0]) ** 2 + ((i[1] + radius) - j[1]) ** 2 <= radius ** 2:
                     self.not_lost_game = True
 
     def meteorits_collision(self):
@@ -332,10 +306,11 @@ class Game:
             sprite_num = 0
         self.image = self.sprites[self.is_force][sprite_num]
         self.rocket = pygame.Rect(self.pos.x, self.pos.y, self.image.get_width(), self.image.get_height())
-        self.hitbox = [(self.rocket.x + self.rocket.width//2 + math.cos((90-sprite_num + i*45)*math.pi/180) * 15, 
-                        self.rocket.y + self.rocket.height//2 + math.sin((90-sprite_num + i*45)*math.pi/180) * 35)
-                       for i in range(8)]
-        pygame.draw.rect(screen, black, self.rocket)
+        self.hitbox = [
+            (self.rocket.x + self.rocket.width // 2 + math.cos((90 - sprite_num + i * 45) * math.pi / 180) * 15,
+             self.rocket.y + self.rocket.height // 2 + math.sin((90 - sprite_num + i * 45) * math.pi / 180) * 35)
+            for i in range(8)]
+        pygame.draw.rect(screen, self.background_color, self.rocket)
         screen.blit(self.image, self.rocket)
 
     def change_rocket_position(self):
@@ -373,9 +348,9 @@ class Game:
 
     def game_over(self, screen):
         while self.not_lost_game:
-            game_over_text = create_text("Game Over", font_preferences, 110, (255, 0, 0))
-            continue_text = create_text("Press space to restart game", font_preferences, 35, (255, 0, 0))
-            back_to_menu = create_text("Press escape to back to menu", font_preferences, 35, (255, 0, 0))
+            game_over_text = create_text("Game Over", self.font, 110, (255, 0, 0))
+            continue_text = create_text("Press space to restart game", self.font, 35, (255, 0, 0))
+            back_to_menu = create_text("Press escape to back to menu", self.font, 35, (255, 0, 0))
 
             self.show_text(screen, game_over_text, 1 / 2, 1 / 4)
             self.show_text(screen, continue_text, 1 / 2, 4 / 11)
@@ -395,13 +370,13 @@ class Game:
                         self.not_lost_game = False
                         self.done = True
             self.clock.tick(self.tick_time)
-    
+
     def game_won(self, screen):
         '''higher productivity = higher LOC'''
         while self.won_game:
-            game_over_text = create_text("You won!", font_preferences, 110, (128, 128, 0))
-            continue_text = create_text("Press space to restart game", font_preferences, 35, (128, 128, 0))
-            back_to_menu = create_text("Press escape to back to menu", font_preferences, 35, (128, 128, 0))
+            game_over_text = create_text("You won!", self.font, 110, (128, 128, 0))
+            continue_text = create_text("Press space to restart game", self.font, 35, (128, 128, 0))
+            back_to_menu = create_text("Press escape to back to menu", self.font, 35, (128, 128, 0))
 
             self.show_text(screen, game_over_text, 1 / 2, 1 / 4)
             self.show_text(screen, continue_text, 1 / 2, 4 / 11)
