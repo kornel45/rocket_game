b05b2951cfef01a1f07fd5bbcdd72b697b1b5069
raczak <kornel.raczak@nokia.com>
2019-01-29 14:06:46 +0100

    game does not lag any more

--- pygame_text_helper.py
+++ pygame_text_helper.py
@@ -6,6 +6,16 @@ import pygame
 
 
 def make_font(fonts, size):
+    """
+    Method responsible for making font of certain size. You can specify name of font, then it will
+    be searched in system fonts. If None then default font will be returned
+    :param fonts: list of fonts
+    :type fonts: list or None
+    :param size: size
+    :type size: int
+    :return: pygame surface representing font
+    :rtype: pygame.font.Font
+    """
     available = pygame.font.get_fonts()
     choices = map(lambda font: font.lower().replace(' ', ''), fonts)
     for choice in choices:
@@ -18,6 +28,13 @@ _cached_fonts = {}
 
 
 def get_font(font, size):
+    """
+    Method responsible for creating (if not already cached) and getting font surface
+    :param font: font name
+    :param size: font size
+    :return: pygame surface representing font
+    rtype: pygame.font.Font
+    """
     global _cached_fonts
     key = str(font) + '|' + str(size)
     font = _cached_fonts.get(key, None)
@@ -31,6 +48,19 @@ _cached_text = {}
 
 
 def create_text(text, fonts, size, color):
+    """
+    Method responsible for rendering image from pygame font surface
+    :param text: text we want to show
+    :type text: str
+    :param fonts: fonts we want to use (first available will be choosen)
+    :type fonts: list
+    :param size: size of text
+    :type size: int
+    :param color: color of text
+    :type color: str
+    :return: image of a rendered pygame font
+    :rtype: pygame.image
+    """
     global _cached_text
     key = '|'.join(map(str, (fonts, size, color, text)))
     image = _cached_text.get(key, None)
