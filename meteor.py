2555130504d981fbd31c42e69b1ad64489ef48c8
Polakiewicz <lukasz.polakiewicz@nokia.com>
2019-01-29 11:20:13 +0100

    Some changes in meteor.py and main.py about meteors

--- meteor.py
+++ meteor.py
@@ -13,6 +13,7 @@ class Meteor:
         self.rotation_direction = rotation_direction
         self.size = size
 
+
     def is_visible(self, max_x, max_y):
         if self.x < - 2 * self.size or self.x > 1.2 * max_x or self.y < -2 * self.size or self.y > 1.2 * max_y:
             return False
@@ -28,3 +29,13 @@ def load_sprites_meteors(folder_path):
             nazwa = "\\meteor_" + str(r) + "_" + str(p) + ".png"
             meteory.append(folder_path + nazwa)
     return [pygame.image.load(x) for x in meteory]
+
+def scaling(sprites, to_size):
+    return [pygame.transform.scale(picture, (to_size, to_size)) for picture in sprites]
+
+meteors70 = load_sprites_meteors(r'meteors')
+meteors60 = scaling(meteors70, 120)
+meteors50 = scaling(meteors70, 100)
+meteors40 = scaling(meteors70, 80)
+meteors30 = scaling(meteors70, 60)
+sprites_of_meteor = [meteors30, meteors40, meteors50, meteors60, meteors70]
