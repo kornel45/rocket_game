454b6f3993b6adb352fb2db5238ff3456323a115
witcher1414 <lukaszpolakiewicz14@gmail.com>
2019-01-29 16:25:15 +0100

    Merge branch 'master' of https://github.com/kornel45/rocket_game
    # Conflicts:
    #	main.py
    #	meteor.py

--- meteor.py
+++ meteor.py
@@ -1,11 +1,12 @@
 import pygame
+from typing import List
 
 
 class Meteor:
     """
     Class represents meteors.
     """
-    def __init__(self, x, y, x_acc, y_acc, rotation, rotation_counter, rotation_speed, rotation_direction, size):
+    def __init__(self, x: int, y: int, x_acc: int, y_acc: int, rotation: int, rotation_counter: int, rotation_speed: int, rotation_direction: int, size: int):
         """
         Initializer of a Meteor class
         :param x: where to start to plot meteor on the x axis
@@ -28,45 +29,48 @@ class Meteor:
         self.rotation_direction = rotation_direction
         self.size = size
 
-    def is_visible(self, max_x, max_y):
+    def is_visible(self, max_x: int, max_y: int) -> bool:
         """
         Check if meteor is in on the screen
         :param max_x: size of the x axis of screen
         :param max_y: size of the y axis of screen
-        :return:
+        :return: is meteor visible or not
         """
         if self.x < - 2 * self.size or self.x > 1.2 * max_x or self.y < -2 * self.size or self.y > 1.2 * max_y:
             return False
         return True
 
 
-def load_first_sprites_meteors(folder_path):
+def load_first_sprites_meteors(folder_path: str) -> List[pygame.Surface]:
     """
     Load sprites of meteors
+    :param folder_path: folder to sprites
+    :return list of images
     """
     meteors = []
     sizes = [70]
     position = [i for i in range(36)]
     for r in sizes:
         for p in position:
-            nazwa = "\\meteor_" + str(r) + "_" + str(p) + ".png"
-            meteors.append(folder_path + nazwa)
+            name = "\\meteor_" + str(r) + "_" + str(p) + ".png"
+            meteors.append(folder_path + name)
     return [pygame.image.load(x) for x in meteors]
 
 
-def scaling(sprites, to_size):
+def scaling(sprites: List[pygame.Surface], to_size: int) -> List[pygame.Surface]:
     """
     Scales images
     :param sprites: what it is scaling
     :param to_size: size of scaling in pixels
-    :return:
+    :return: list of scaled images
     """
     return [pygame.transform.scale(picture, (to_size, to_size)) for picture in sprites]
 
 
-def load_all_sprites_of_meteors(folder_path):
+def load_all_sprites_of_meteors(folder_path: str) -> List[List[pygame.Surface]]:
     """
     Load sprites of meteors and scales it.
+    :return list of list, where each list contains scaled images
     """
     meteors70 = load_first_sprites_meteors(folder_path)
     meteors60 = scaling(meteors70, 120)
